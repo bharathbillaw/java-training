2b45445f1056edb77eef288e6a84088c4ff88eba
Bharat Billawa <bharat.b@surya-soft.com>
2018-07-27 18:31:04 +0530

    coading guideline implimented

--- src/com/company/Time.java
+++ src/com/company/Time.java
@@ -2,43 +2,44 @@ package com.company;
 import java.util.Scanner;
 
 public class Time {
-    public int days,hours,minutes,seconds;
+    public int days;
+    public int hours;
+    public int minutes;
+    public int seconds;
 
     Time(){
         Scanner in = new Scanner(System.in);
         System.out.println("Enter time in seconds:");
-        int seconds_in = in.nextInt();
-
-        days = (int) ( seconds_in/(60*60*24) );
-        seconds = seconds_in - (days*(60*60*24));
+        int secondsin = in.nextInt();
+        days = (int) ( secondsin/(60*60*24) );
+        seconds = secondsin - (days*(60*60*24));
         hours = (int) seconds /(60*60);
         seconds = seconds - (hours * (60*60));
         minutes = (int) (seconds/60);
         seconds = seconds - (minutes*60);
-
     }
 
     public void printOnlyDays(){
-        float all_days;
-        all_days = days + (float)  (hours/24) +(float)  minutes/(60*24) +(float)  seconds/(60*60*24);
-        System.out.println(all_days + " days");
+        float buff;
+        buff = days + (float)  (hours/24) +(float)  minutes/(60*24) +(float)  seconds/(60*60*24);
+        System.out.println(buff + " days");
     }
 
     public void printOnlyHours(){
-        float all_days;
-        all_days = days*24 +  hours +(float)  minutes/(60) +(float)  seconds/(60*60);
-        System.out.println(all_days + " hours");
+        float buff;
+        buff = days*24 +  hours +(float)  minutes/(60) +(float)  seconds/(60*60);
+        System.out.println(buff + " hours");
     }
 
     public void printOnlyMinutes(){
-        float all_days;
-        all_days = days*24*60 +  hours*60 + minutes +(float)  seconds/(60);
-        System.out.println(all_days + " minutes");
+        float buff;
+        buff = days*24*60 +  hours*60 + minutes +(float)  seconds/(60);
+        System.out.println(buff + " minutes");
     }
 
     public void printOnlySeconds(){
-        int all_days;
-        all_days = days*24*60*60 +  hours*60*60 + minutes*60 + seconds ;
-        System.out.println(all_days);
+        int buff;
+        buff = days*24*60*60 +  hours*60*60 + minutes*60 + seconds ;
+        System.out.println(buff);
     }
 }
