506af8f290621a10bb73150719867d1c995d96db
Bharat Billawa <bharat.b@surya-soft.com>
2018-07-27 17:39:00 +0530

    Program2 (on branch prg2): To print flag

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,51 @@
 package com.company;
 
-import java.io.IOException;
-import java.util.Scanner;
+/* Write a Java program to print an American flag on the screen. Go to the editor
+         Expected Output 34 46 15 9
+
+         * * * * * * ==================================
+          * * * * *  ==================================
+         * * * * * * ==================================
+          * * * * *  ==================================
+         * * * * * * ==================================
+          * * * * *  ==================================
+         * * * * * * ==================================
+          * * * * *  ==================================
+         * * * * * * ==================================
+         ==============================================
+         ==============================================
+         ==============================================
+         ==============================================
+         ==============================================
+         ==============================================  */
 
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        table a = new table();
+    public static void printStar(int n) {
+        for (int i = 0; i < n; i++) System.out.print("* "); // Prints * with a space after it
+    }
 
-        System.out.println(a.var);
+    public static void printLine(int n) {
+        for (int i = 0; i < n; i++) System.out.print("="); // Prints * with a space after it
+        System.out.println("\n");
+    }
 
-        a.printTable();
+    public static void main(String[] args){
 
+        for(int i=1;i<10;i++){
+            if(i%2 != 0) printStar(6);
+            else {
+                System.out.print(" ");
+                printStar(5);
+                System.out.print(" ");// prints an extra space
+            }
+            printLine(34);
+        }
 
+        for(int i=0;i<6;i++){
+            printLine(46);
+        }
 
     }
 }
