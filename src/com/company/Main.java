609f0011b7c086cb68739ab08c4d7007cb9d4473
Bharat Billawa <bharat.b@surya-soft.com>
2018-07-27 17:35:38 +0530

    Program 1 (in master): to print multiplication table

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        table a = new table();
+
+        System.out.println(a.var);
+
+        a.printTable();
+
+
+
+    }
+}
